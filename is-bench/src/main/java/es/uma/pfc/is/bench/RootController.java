f94c42aed080bbf456bbdc32cca173e9bc87368b
doramcalderon <doramcalderon@users.noreply.github.com>
2015-05-06 20:43:51 +0200

    Preferencias de Usuario: * Ventana modal

--- is-bench/src/main/java/es/uma/pfc/is/bench/RootController.java
+++ is-bench/src/main/java/es/uma/pfc/is/bench/RootController.java
@@ -15,6 +15,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +27,9 @@ public class RootController implements Initializable {
     @FXML
     private Menu preferencesMenu;
 
+    @FXML
+    private BorderPane rootPane;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("initialize");   
@@ -41,6 +46,8 @@ public class RootController implements Initializable {
             Scene preferencesScene = new Scene(configView);
             
             Stage preferencesStage = new Stage();
+            preferencesStage.initOwner(rootPane.getScene().getWindow());
+            preferencesStage.initModality(Modality.WINDOW_MODAL);
             preferencesStage.setTitle(" Workspaces");
             preferencesStage.centerOnScreen();
             preferencesStage.setScene(preferencesScene);
