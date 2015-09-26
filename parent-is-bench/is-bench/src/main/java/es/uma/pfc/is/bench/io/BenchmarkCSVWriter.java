f0e93696c255bf512d8521ac49ee04a3b27232fd
doramcalderon <doramcalderon@users.noreply.github.com>
2015-09-26 12:05:10 +0200

    Resultados.

---
+++ parent-is-bench/is-bench/src/main/java/es/uma/pfc/is/bench/io/BenchmarkCSVWriter.java
@@ -0,0 +1,96 @@
+package es.uma.pfc.is.bench.io;
+
+import es.uma.pfc.is.algorithms.AlgorithmResult;
+import es.uma.pfc.is.algorithms.io.CSVFileWriter;
+import es.uma.pfc.is.bench.domain.BenchmarkResult;
+import es.uma.pfc.is.commons.files.FileUtils;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Writes to CSV a benchmarks results.
+ *
+ * @author Dora Calderón
+ */
+public class BenchmarkCSVWriter {
+
+    /**
+     * CSV Writer.
+     */
+    private static CSVFileWriter csvWriter;
+
+    /**
+     * Prints a benchmark results to a CSV file.
+     *
+     * @param result Benchmark result.
+     * @param outputPath CSV file otuput path.
+     * @throws IOException
+     */
+    public static void print(BenchmarkResult result, Path outputPath) throws IOException {
+        if (result != null) {
+             Map<Class, List<AlgorithmResult>> resultsGrouped = result.getAlgorithmResults().stream()
+                .collect(Collectors.groupingBy(AlgorithmResult::getAlgorithmClass));
+            int inputsCount = resultsGrouped.values().stream().findFirst().get().size();
+            
+            csvWriter = new CSVFileWriter(outputPath);
+            addHeaders(inputsCount);
+            csvWriter.start();
+            printResultsByAlgorithm(resultsGrouped);
+            csvWriter.finish();
+        }
+    }
+
+    /**
+     * Adds the headers.<br/>
+     * These will be: "Algorithm", "Input1", "Size1", "Card1", "Input2", "Size2", "Card2", ...,"InputN", "SizeN",
+     * "CardN" where N is the results number by algorithm.
+     *
+     * @param resultsNumber Results number.
+     */
+    protected static void addHeaders(int resultsNumber) {
+        // add headers
+        List<String> headers = new ArrayList<>();
+        headers.add("Algorithm");
+
+        for (int index = 1; index <= resultsNumber; index++) {
+            headers.add("Input" + index);
+            headers.add("Size" + index);
+            headers.add("Card" + index);
+        }
+        csvWriter.header(headers.toArray(new String[]{}));
+    }
+
+    /**
+     * Prints a row by result algorithm.
+     *
+     * @param resultsGrouped Benchmark results grouped by algorithm.
+     * @throws IOException
+     */
+    protected static void printResultsByAlgorithm(Map<Class, List<AlgorithmResult>> resultsGrouped) throws IOException {
+        
+        List<AlgorithmResult> currentResults;
+        List<String> fields = new ArrayList<>();
+
+        for (Class algClass : resultsGrouped.keySet()) {
+            fields.clear();
+            fields.add(algClass.getSimpleName());
+
+            currentResults = resultsGrouped.get(algClass);
+            if (currentResults != null && !currentResults.isEmpty()) {
+                for (AlgorithmResult r : currentResults) {
+                    fields.add(FileUtils.getName(r.getInputFile()));
+                    fields.add(String.valueOf(r.getSize()));
+                    fields.add(String.valueOf(r.getCardinality()));
+                }
+
+            }
+            csvWriter.printRecord(fields.toArray());
+        }
+    }
+
+}
--- parent-is-bench/is-bench/src/main/java/es/uma/pfc/is/bench/services/AlgorithmExecService.java
+++ parent-is-bench/is-bench/src/main/java/es/uma/pfc/is/bench/services/AlgorithmExecService.java
@@ -3,27 +3,23 @@ package es.uma.pfc.is.bench.services;
 import es.uma.pfc.is.algorithms.Algorithm;
 import es.uma.pfc.is.algorithms.AlgorithmExecutor;
 import es.uma.pfc.is.algorithms.AlgorithmOptions;
+import es.uma.pfc.is.algorithms.AlgorithmOptions.Mode;
 import es.uma.pfc.is.algorithms.AlgorithmOptions.Options;
 import es.uma.pfc.is.algorithms.AlgorithmResult;
-import es.uma.pfc.is.algorithms.io.CSVFileWriter;
-import es.uma.pfc.is.algorithms.util.ImplicationalSystems;
 import es.uma.pfc.is.bench.benchmarks.execution.RunBenchmarkModel;
 import es.uma.pfc.is.bench.domain.AlgorithmEntity;
 import es.uma.pfc.is.bench.domain.BenchmarkResult;
 import es.uma.pfc.is.commons.eventbus.Eventbus;
 import es.uma.pfc.is.bench.events.MessageEvent;
-import es.uma.pfc.is.commons.files.FileUtils;
-import es.uma.pfc.is.logging.AlgorithmLogger;
-import fr.kbertet.lattice.ImplicationalSystem;
+import es.uma.pfc.is.bench.io.BenchmarkCSVWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
@@ -102,30 +98,52 @@ public class AlgorithmExecService extends Service<BenchmarkResult> {
         options.addOption(Options.OUTPUT_TYPE, model.getOutputFileType());
         return options;
     }
+    
+    /**
+     * Prints the results into a CSV file.
+     * @param result Results.
+     * @throws IOException 
+     */
+    protected void printResults(BenchmarkResult result) throws IOException {
+        Path csvFile = Paths.get(model.getOutputDir(), model.getSelectedBenchmark().getName() + ".csv");
+        BenchmarkCSVWriter.print(result, csvFile);
+        Eventbus.post(new MessageEvent("The execution has finished succeeded.", MessageEvent.Level.SUCCEEDED));
+    }
 
+    /**
+     * Task for execute algorithms and benchmarks.<br/>
+     * If the {@link Mode#STATISTICS} mode is enabled, prints the results into a CSV file.
+     * @return Task
+     */
     @Override
-
     protected Task<BenchmarkResult> createTask() {
         return new Task<BenchmarkResult>() {
 
             @Override
             protected BenchmarkResult call() throws Exception {
+                BenchmarkResult benchmarkResult = null;
                 instanceAlgorithms();
                 List<AlgorithmResult> results = new ArrayList<>();
                 Date timeStamp = new Date();
                 
                 if (algorithms != null) {
                     String [] inputs = model.getSelectedInputFiles().toArray(new String[]{});
-                    
+                    AlgorithmOptions options = getOptions();
                     AlgorithmExecutor exec = new AlgorithmExecutor()
                                                 .inputs(inputs)
-                                                .options(getOptions());
+                                                .options(options);
                     algorithms.stream().filter((alg) -> (alg != null)).forEach((alg) -> {
                         results.addAll(exec.execute(alg));
                     });
                     
+                    benchmarkResult = new BenchmarkResult(results, timeStamp);
+                    if(options.isEnabled(Mode.STATISTICS)) {
+                        printResults(benchmarkResult);
+                    }
                 }
-                return new BenchmarkResult(results, timeStamp);
+                
+            
+                return benchmarkResult;
             }
         };
     }
@@ -148,69 +166,11 @@ public class AlgorithmExecService extends Service<BenchmarkResult> {
         Eventbus.post(new MessageEvent("The execution has failed: " + getException().getMessage(), MessageEvent.Level.ERROR));
     }
 
+    /**
+     * If statistics mode is enabled, prints the benchmark results into a CSV file.
+     */
     @Override
     protected void succeeded() {
-        AlgorithmOptions options = getOptions();
-        options.addOption(Options.OUTPUT, model.getOutputDir());
-        options.addOption(Options.LOG_BASE_NAME, model.getSelectedBenchmark().getName());
-        AlgorithmLogger logger = new AlgorithmLogger(model.getSelectedBenchmark().getName(), options, true);
-        BenchmarkResult results = this.getValue();
-        if(results != null) {
-            CSVFileWriter csvWriter = null;
-//            try {
-                csvWriter = new CSVFileWriter(Paths.get(model.getOutputDir(), model.getSelectedBenchmark().getName() + ".csv"));
-
-                // add headers
-                List<String> headers = new ArrayList<>();
-                headers.add("Algorithm");
-
-                model.getSelectedInputFiles().stream().forEach((input) -> {
-                    headers.add("Input");
-                    headers.add("Size");
-                    headers.add("Card");
-                });
-            try {
-                csvWriter.header(headers.toArray(new String[]{}));
-                csvWriter.start();
-            } catch (IOException ex) {
-                Logger.getLogger(AlgorithmExecService.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-                Map<Class, List<AlgorithmResult>> resultsGrouped = results.getAlgorithmResults().stream()
-                        .collect(Collectors.groupingBy(AlgorithmResult::getAlgorithmClass));
-
-                List<AlgorithmResult> currentResults;
-                List<String> fields = new ArrayList<>();
-
-                for (Class algClass : resultsGrouped.keySet()) {
-                    fields.clear();
-                    fields.add(algClass.getSimpleName());
-
-                    currentResults = resultsGrouped.get(algClass);
-                    if (currentResults != null && !currentResults.isEmpty()) {
-                        for (AlgorithmResult r : currentResults) {
-                            fields.add(FileUtils.getName(r.getInputFile()));
-                            fields.add(String.valueOf(r.getSize()));
-                            fields.add(String.valueOf(r.getCardinality()));
-                        }
-
-                    }
-                    try {
-                        csvWriter.printRecord(fields.toArray());
-                    } catch (IOException ex) {
-                        Logger.getLogger(AlgorithmExecService.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-//            } catch (IOException ex) {
-//                Logger.getLogger(AlgorithmExecService.class.getName()).log(Level.SEVERE, null, ex);
-//            } finally {
-//                if(csvWriter != null) {
-//                    csvWriter.finish();
-//                }
-//            }
-
-        }
-        
         Eventbus.post(new MessageEvent("The execution has finished succeeded.", MessageEvent.Level.SUCCEEDED));
     }
 
